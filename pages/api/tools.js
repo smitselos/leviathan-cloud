d7e8c00f5966e8734e732d101acadf8d1b112ffb
smitselos <smitselos@gmail.com>
2026-02-25 19:29:04 +0200

    Update tools.js

--- pages/api/tools.js
+++ pages/api/tools.js
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from './auth/[...nextauth]';
-import { listToolsFromDrive } from '../../../lib/drive';
+import { listToolsFromDrive } from '../../lib/drive';
 
 export default async function handler(req, res) {
   const session = await getServerSession(req, res, authOptions);
